0b12a4cd2f03cc288659d87f4f7d2828f39ba3d8
NaderCc <kanmagdy@gmail.com>
2026-05-24 00:49:20 +0300

    try webhook

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This is some data from the API' });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
